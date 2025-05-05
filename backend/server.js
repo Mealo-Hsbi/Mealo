5d5ce061bf9edcb307bf994b7fd366a5041c8515
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2025-05-06 01:05:15 +0200

    Implement basic server structure

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const https = require('https');
+const fs = require('fs');
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+const options = {
+  key: fs.readFileSync('./certs/key.pem'),
+  cert: fs.readFileSync('./certs/cert.pem'),
+};
+
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`HTTPS-Server läuft auf https://localhost:${PORT}`);
+});
