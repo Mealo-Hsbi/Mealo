1e53659cb93b5fb704d57ac86d5f2fe2167045f4
Bjarne Zaremba <bjarne.zaremba@gmail.com>
2025-05-28 00:19:21 +0200

    refactor(profile.controller): remove console log from getProfile method

--- backend/app/controllers/profile.controller.js
+++ backend/app/controllers/profile.controller.js
@@ -4,7 +4,6 @@ exports.getProfile = async (req, res, next) => {
   try {
     // auth.middleware hat req.user.uid gesetzt
     const profileDto = await profileService.fetchProfile(req.user.uid);
-    console.log('Profile fetched:', profileDto);
     res.json(profileDto);
   } catch (err) {
     next(err);
